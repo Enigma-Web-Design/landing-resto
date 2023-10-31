2bd46f1ec0e5898e86791cc6067704bf4cdf2855
leonstefano03 <leonstefano03@gmail.com>
2023-10-31 20:27:30 -0300

    nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,19 @@
-import Image from 'next/image'
-import yourResto from '../../public/yourRest.jpg'
+import Image from 'next/image';
+import yourResto from '../../public/your_logo.png';
 export default function Nav() {
-
-
-
   return (
-    <nav className="bg-white flex w-full p-2 text-black justify-center">
-<a className="text-center bg-yellow-400">
- <Image width={80} src={yourResto} alt='logo'/>
- </a>
+    <nav className="bg-white flex w-full p-2 text-black">
+      <ul className="flex items-center justify-around w-full text-sm font-semibold">
+        <li className="hover:text-yellow-400">ABOUT</li>
+        <li className="hover:text-yellow-400">MENUS</li>
+        <li className="hover:text-yellow-400">LOREM</li>
+        <li>
+          <Image width={80} src={yourResto} alt="logo" />
+        </li>
+        <li className="hover:text-yellow-400">LOREM</li>
+        <li className="hover:text-yellow-400">LOREM</li>
+        <li className="hover:text-yellow-400">LOREM</li>
+      </ul>
     </nav>
-  )
+  );
 }
