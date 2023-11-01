895252e0feb3e9d4938ec9410fd9ad4e27e1ff79
leonstefano03 <leonstefano03@gmail.com>
2023-11-01 17:05:25 -0300

    updating-feat:creating-section

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,19 +1,19 @@
 export default function Section({ position, foodBack }) {
   return (
     <section
-      className={`w-full h-[500px] bg-white text-black flex ${position} bg-cover bg-center`}
+      className={`w-full h-[500px] text-black flex ${position} bg-cover bg-top border-b-black border-b `}
       style={{ backgroundImage: `url(${foodBack})` }}
     >
-      <div className="flex flex-col w-1/2 p-12 justify-center items-center bg-white bg-opacity-80">
+      <div className="flex flex-col w-full p-12 sm:w-1/2 justify-center items-center bg-white bg-opacity-30 sm:bg-opacity-0 text-center ">
         <small className="font-bold text-base">JOIN TODAY</small>
-        <h2 className="font-bold text-3xl mb-4">JOIN THE REAL DEAL</h2>
-        <p className="leading-8 text-lg text-center">
+        <h2 className="font-extrabold text-3xl mb-4">JOIN THE REAL DEAL</h2>
+        <p className="leading-8 text-lg font-medium">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
           necessitatibus aperiam nihil incidunt iure laborum similique
           doloremque, magnam totam sed nemo libero, velit adipisci natus fuga
           cumque dolorum quas doloribus.
         </p>
-        <button className="font-bold text-base px-12 py-2 my-6 outline-black outline-2 outline hover:outline-8">
+        <button className="font-extrabold text-base px-12 py-2 my-6 outline-black outline-2 outline hover:outline-8">
           JOIN HERE
         </button>
       </div>
