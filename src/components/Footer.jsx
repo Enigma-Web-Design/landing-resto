52398cbb2ff21f8d023df0dcd784c289d45cad52
valentin <bellberger45@gmail.com>
2023-11-01 16:11:31 -0300

    footer 1.0

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,71 +1,83 @@
+import { FaTwitter, FaFacebook, FaInstagram } from "react-icons/fa";
+
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 text-white p-4 absolute bottom-0 w-full h-3/6 flex flex-row py-20 justify-center">
-      <div className="flex mr-20">
-        <div className="left-content mx-10">
-          <h3 className="font-bold"> Opciones adicionales:</h3>
-          <ul className="list-disc pl-4">
-            <li className="my-4">Opción 1</li>
-            <li className="my-4">Opción 2</li>
-            <li className="my-4">Opción 3</li>
-            <li className="my-4">Opción 4</li>
-            <li className="my-4">Opción 5</li>
-            <li className="my-4">Opción 6</li>
-            <li className="my-4">Opción 7</li>
-          </ul>
-        </div>
-        <div className="left-content mx-10">
-          <h3 className="font-bold">Opciones adicionales:</h3>
-          <ul className="list-disc pl-4">
-            <li className="my-4">Opción 1</li>
-            <li className="my-4">Opción 2</li>
-            <li className="my-4">Opción 3</li>
-            <li className="my-4">Opción 4</li>
-            <li className="my-4">Opción 5</li>
-            <li className="my-4">Opción 6</li>
-          </ul>
-        </div>
-        <div className="left-content mx-10">
-          <h3 className="font-bold">Opciones adicionales:</h3>
-          <ul className="list-disc pl-4">
-            <li className="my-4">Opción 1</li>
-            <li className="my-4">Opción 2</li>
-            <li className="my-4">Opción 3</li>
-            <li className="my-4">Opción 4</li>
-          </ul>
-        </div>
-      </div>
-      <div className="right-content mx-20   justify-between flex flex-col">
-        <div className="flex flex-col justify-center  ">
-          <a href="/terminos" className="font-bold text-l">
-            CALL US
-          </a>
-          <a href="/terminos" className="font-bold text-3xl">
-            {"("}xx{")"}-xxx-xxx
-          </a>
-        </div>
-
-        <div className="mb-5">
-          <p className="border border-4 py-5 px-10 text-center">book a table</p>
-        </div>
-
-        <div className="flex flex-col justify-center  ">
-          <a href="/terminos" className="font-bold ">
-            Follow Us
-          </a>
-          <div className="">
-            <a className="mr-2" href="https://www.facebook.com">
-              Facebook
-            </a>
-            <a className="mr-2" href="https://www.twitter.com">
-              Twitter
-            </a>
-            <a className="mr-2" href="https://www.instagram.com">
-              Instagram
-            </a>
+    <footer className="bg-gray-800 text-white  bottom-0 p-4 w-full">
+      <div className="container mx-auto flex flex-col md:flex-row justify-between py-10">
+        <div className="md:flex">
+          <div className="left-content mx-4 md:mx-10">
+            <h3 className="font-bold">Opciones adicionales:</h3>
+            <ul className="list-none">
+              <li className="my-2 md:my-4">Opción 1</li>
+              <li className="my-2 md:my-4">Opción 2</li>
+              <li className="my-2 md:my-4">Opción 3</li>
+              <li className="my-2 md:my-4">Opción 4</li>
+              <li className="my-2 md:my-4">Opción 5</li>
+              <li className="my-2 md:my-4">Opción 6</li>
+              <li className="my-2 md:my-4">Opción 7</li>
+            </ul>
+          </div>
+          <div className="left-content mx-4 md:mx-10">
+            <h3 className="font-bold">Opciones adicionales:</h3>
+            <ul className="list-none">
+              <li className="my-2 md:my-4">Opción 1</li>
+              <li className="my-2 md:my-4">Opción 2</li>
+              <li className="my-2 md:my-4">Opción 3</li>
+              <li className="my-2 md:my-4">Opción 4</li>
+              <li className="my-2 md:my-4">Opción 5</li>
+              <li className="my-2 md:my-4">Opción 6</li>
+            </ul>
+          </div>
+          <div className="left-content mx-4 md:mx-10">
+            <h3 className="font-bold">Opciones adicionales:</h3>
+            <ul className="list-none">
+              <li className="my-2 md:my-4">Opción 1</li>
+              <li className="my-2 md:my-4">Opción 2</li>
+              <li className="my-2 md:my-4">Opción 3</li>
+              <li className="my-2 md:my-4">Opción 4</li>
+            </ul>
           </div>
         </div>
-        <div>&copy; 2023 Mi Sitio Web</div>
+        <div className="right-content mb-10 md:mt-0 md:flex flex-col justify-center">
+          <div className="flex flex-col justify-center items-center md:items-start">
+            <a href="/terminos" className="font-bold text-lg md:text-xl">
+              CALL US
+            </a>
+            <a href="/terminos" className="font-bold text-3xl my-2">
+              (xx)-xxx-xxx
+            </a>
+            <div className="my-10 mb-15">
+              <p className="hover:outline-8 outline outline-4 py-2 px-4 text-center text-lg">
+                Book a Table
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col justify-center items-center md:items-start mt-4 md:mt-0">
+            <a href="/terminos" className="font-bold text-lg">
+              Follow Us
+            </a>
+            <div className="flex mt-2">
+              <a
+                className="my-2 mr-2 border-2 p-1"
+                href="https://www.facebook.com"
+              >
+                <FaFacebook />
+              </a>
+              <a
+                className="my-2 mx-2 border-2 p-1"
+                href="https://www.twitter.com"
+              >
+                <FaTwitter />
+              </a>
+              <a
+                className="my-2 mx-2 border-2 p-1"
+                href="https://www.instagram.com"
+              >
+                <FaInstagram />
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </footer>
   );
