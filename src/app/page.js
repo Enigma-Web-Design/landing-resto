efddce71a6239135286409b718c771f882198c1c
valentin <bellberger45@gmail.com>
2023-11-02 15:53:15 -0300

    main alpha

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,18 @@
-import Article from '@/components/Article';
-import Section from '@/components/Section';
+import Article from "@/components/Article";
+import Section from "@/components/Section";
 import Footer from "@/components/Footer";
 import Grid from "@/components/Grid";
 import Nav from "@/components/Nav";
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <>
       <Nav />
-      <Grid />      <Article foodBack={'/owner.png'} />
-      <Section position={'justify-end bg-center'} foodBack={'/food.png'} />
-      <Section position={'justify-start'} foodBack={'/vip.jpg'} />
+      <Header />
+      <Grid /> <Article foodBack={"/owner.png"} />
+      <Section position={"justify-end bg-center"} foodBack={"/food.png"} />
+      <Section position={"justify-start"} foodBack={"/vip.jpg"} />
       <Footer />
     </>
   );
