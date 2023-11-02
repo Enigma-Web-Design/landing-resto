8766ea3f4b2aaa61074a2b8a77f4bd97fcd9f7ac
valentin <bellberger45@gmail.com>
2023-11-02 16:29:12 -0300

    fixed index main

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,14 +12,14 @@ const Header = () => {
 
   return (
     <>
-      <div className="flex flex-col items-center justify-center  absolute text-white z-10 w-full h-[670px] xl:h-[640px] lg:h-[600px] md:h-[540px] sm:h-[500px]  color-white-800">
+      <div className="flex flex-col items-center justify-center  absolute text-white z-[-1] w-full h-[670px] xl:h-[640px] lg:h-[600px] md:h-[540px] sm:h-[500px]  color-white-800">
         <h1 className="text-[70px]  sm:text-[90px] font-bold">WELCOME</h1>
         <h2>to the oasis of the lunch</h2>
         <button className="mt-10 font-bold border p-2 px-20 border-4 outline outline-white outline-4 border-transparent duration-100 hover:border-white ">
           BOOK A TABLE
         </button>
       </div>
-      <header className="h-[670px] xl:h-[640px] lg:h-[600px] md:h-[540px] sm:h-[500px]  relative z-[-1] bg-gray-900 overflow-hidden">
+      <header className="h-[670px] xl:h-[640px] lg:h-[600px] md:h-[540px] sm:h-[500px]  relative z-[-2] bg-white-900 overflow-hidden">
         <Swiper
           effect={"cube"}
           grabCursor={true}
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import yourResto from "../../public/your_logo.png";
 export default function Nav() {
   return (
-    <nav className="bg-white absolute flex w-full p-2 text-black sticky top-0 z-100 backdrop-blur-sm bg-opacity-90 ">
+    <nav className="bg-white  flex w-full p-2 text-black sticky top-0 z-100 backdrop-blur-sm bg-opacity-90 ">
       <ul className="flex items-center justify-around w-full text-sm font-semibold sm:mx-24">
         <li className="hover:text-green-700 cursor-pointer">ABOUT</li>
         <li className="hover:text-green-700 cursor-pointer">MENUS</li>
