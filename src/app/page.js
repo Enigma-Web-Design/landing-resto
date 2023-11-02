5391125052b504b822b98ef07d5537cc2e57ea4d
leonstefano03 <leonstefano03@gmail.com>
2023-11-02 18:21:39 -0300

    feat:first-article

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,21 @@
-import Article from "@/components/Article";
-import Section from "@/components/Section";
-import Footer from "@/components/Footer";
-import Grid from "@/components/Grid";
-import Nav from "@/components/Nav";
-import Header from "@/components/Header";
+import Article from '@/components/Article';
+import Section from '@/components/Section';
+import Footer from '@/components/Footer';
+import Grid from '@/components/Grid';
+import Nav from '@/components/Nav';
+import Header from '@/components/Header';
+import ArticleOwner from '@/components/ArticleOwner';
 
 export default function Home() {
   return (
     <>
       <Nav />
       <Header />
-      <Grid /> <Article foodBack={"/owner.png"} />
-      <Section position={"justify-end bg-center"} foodBack={"/food.png"} />
-      <Section position={"justify-start"} foodBack={"/vip.jpg"} />
+      <Article/>
+      <Grid /> 
+      <ArticleOwner foodBack={'/owner.png'} />
+      <Section position={'justify-end bg-center'} foodBack={'/food.png'} />
+      <Section position={'justify-start'} foodBack={'/vip.jpg'} />
       <Footer />
     </>
   );
